3ce7e4b0b9ac6c31dcceab6e22e138d10c16116c
mativasca <166620939+mativasca@users.noreply.github.com>
2024-04-19 17:30:48 -0300

    Update login.php
    Cambio por nueva interfaz

--- login.php
+++ login.php
@@ -11,52 +11,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/login.css">
-    <!-- <script src="https://kit.fontawesome.com/600e7f7446.js" crossorigin="anonymous"></script> Comento, no es necesario y sale la ficha de donde salieron los iconos -->
-    <title>Iniciar sesión</title> <!-- Cambio Mati -->
+    <title>Iniciar sesión</title>
+	<link rel="stylesheet" type="text/css" href="css/login2.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 
 </head>
 
 <body>
     <header>
         <div>
-            <!-- <nav>
-                <div>
-                <a href="index.php" class="header_div_nav-item">Inicio</a>
-                <div>
-            </nav> -->
+
         </div>
     </header>
-    <main>
-        <div class="contenedorLogin">
-            <form class="contenedorLogin-form" method="POST">
-                <label class="contenedorLogin_form-label" for="dni">
-                    Ingrese su dni
-                    <input class="contenedorLogin_form_label-input" type="number" name="dni" id="dni" placeholder="Ingrese su DNI" maxlength="8" required> <!-- modificado Mati el placeholder-->
-                </label>
-                <label class="contenedorLogin_form-label" for="contra">
-                    Ingrese su contraseña
-                    <input class="contenedorLogin_form_label-input" type="password" name="contrasenia" id="contra" placeholder="Ingrese su contraseña" required> <!-- modificado Mati el placeholder-->
-                </label>
-                <div class="contenedorLogin_form-cajaBtn">
-                    <button class="btn-ok" type="submit">Acceder</button>
-                </div>
-            </form>
-            <div class="contenedorLogin-cajaMensaje">
-                <p class="contenedorLogin_cajaMensaje-Texto">
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Iniciar sesión</label>
+				</form>
+			</div>
+
+			<div class="login">
+				<form method="POST">
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="number" name="dni" id="dni" placeholder="Ingrese su DNI" required="">
+					<input type="password" name="contrasenia" id="contra" placeholder="Contraseña" required="">
+					<button>Acceder</button>
+				</form>
+			</div>
+	</div>
+
                     <?php 
                         if( isset($_POST['dni']) )
                             Usuario::VerificarUsuario($_POST['dni'],$_POST['contrasenia'])
                     ?>
-                </p>
-            </div>
-        </div>
+
+
     </main>
-    <footer>
-        <font-size="5"><h4><p class="titulos blanco" ><font-size="5"></p><br>
-        <p class="titulos blanco"></p></h4></font-size>
-    </footer>
+
 </body>
 
 </html>
