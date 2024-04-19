f3730f1b5fa021dc045ef69ef78216fa71874923
mativasca <166620939+mativasca@users.noreply.github.com>
2024-04-19 15:25:24 -0300

    Update pantalla_carreras.php

---
+++ pantalla_carreras.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "database\conectar_db.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Bienvenido</title>
+</head>
+<header>
+        <p class="titulos blanco">Seleccione una materia</p>
+</header>
+<main>
+
+<body>
+
+
+</body>
+</main>
+<footer>
+        <font-size="2"><h4><p class="titulos blanco" ><font-size="2"></p><br>
+        <p class="titulos blanco"></p></h4></font-size>
+    </footer>
+</html>
