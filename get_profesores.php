ad07225aab638ecaff45febf84917a89f657b186
Fer <fernando.alberto.lemos@gmail.com>
2024-07-05 14:39:34 -0300

    agrego nombre al profesor

--- get_profesores.php
+++ get_profesores.php
@@ -5,7 +5,7 @@ if (isset($_POST['carrera'])) {
     $carrera_id = $_POST['carrera'];
     $con = conectar_db();
 
-    $sql_profesor = "SELECT DISTINCT profesores.profesor_id, profesores.profesor_apellido 
+    $sql_profesor = "SELECT DISTINCT profesores.profesor_id, profesores.profesor_apellido, profesores.profesor_nombre  
                     FROM profesores 
                     INNER JOIN materia_carrera ON profesores.profesor_id = materia_carrera.profesor_id
                     WHERE materia_carrera.carrera_id = ?";
@@ -17,7 +17,8 @@ if (isset($_POST['carrera'])) {
 
     echo "<option value=''>Seleccione un profesor</option>";
     while ($fila = $resultado_profesor->fetch_assoc()) {
-        echo "<option value='{$fila['profesor_id']}'>{$fila['profesor_apellido']}</option>";
+        $profesor = $fila['profesor_nombre'].' '. $fila['profesor_apellido'];
+        echo "<option value='{$fila['profesor_id']}'>{$profesor}</option>";
     }
     $stmt->close();
     $con->close();
