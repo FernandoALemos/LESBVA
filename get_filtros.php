047a790ec436d888821c9021d0abc99fb074bf5e
Fer <fernando.alberto.lemos@gmail.com>
2024-09-23 20:13:22 -0300

    Fix orden datos

--- get_filtros.php
+++ get_filtros.php
@@ -2,83 +2,59 @@
 require_once "database/conectar_db.php";
 $con = conectar_db();
 
-$ciclo = $_POST['ciclo'] ?? '';
-$carrera = $_POST['carrera'] ?? '';
-$turno = $_POST['turno'] ?? '';
-$curso = $_POST['curso'] ?? '';
-$profesor = $_POST['profesor'] ?? '';
-
-$where_clauses = [];
-if (!empty($ciclo)) {
-    $where_clauses[] = "mc.ciclo_id = '$ciclo'";
-}
-if (!empty($carrera)) {
-    $where_clauses[] = "mc.carrera_id = '$carrera'";
-}
-if (!empty($turno)) {
-    $where_clauses[] = "mc.turno_id = '$turno'";
-}
-if (!empty($curso)) {
-    $where_clauses[] = "mc.curso_id = '$curso'";
-}
-if (!empty($profesor)) {
-    $where_clauses[] = "mc.profesor_id = '$profesor'";
+// 1. Ciclos (orden descendente)
+$sql_ciclos = "SELECT DISTINCT cl.ciclo_id, cl.ciclo 
+            FROM ciclo_lectivo cl 
+            INNER JOIN materia_carrera mc ON mc.ciclo_id = cl.ciclo_id
+            ORDER BY cl.ciclo DESC";
+$resultado_ciclos = $con->query($sql_ciclos);
+$ciclos = [];
+while ($fila = $resultado_ciclos->fetch_assoc()) {
+    $ciclos[] = ['id' => $fila['ciclo_id'], 'descripcion' => $fila['ciclo']];
 }
 
-$where_sql = '';
-if (!empty($where_clauses)) {
-    $where_sql = 'WHERE ' . implode(' AND ', $where_clauses);
+// 2. Carreras (orden alfabético ascendente)
+$sql_carreras = "SELECT DISTINCT c.carrera_id, c.carrera_nombre 
+            FROM carreras c
+            INNER JOIN materia_carrera mc ON mc.carrera_id = c.carrera_id
+            ORDER BY c.carrera_nombre ASC";
+$resultado_carreras = $con->query($sql_carreras);
+$carreras = [];
+while ($fila = $resultado_carreras->fetch_assoc()) {
+    $carreras[] = ['id' => $fila['carrera_id'], 'descripcion' => $fila['carrera_nombre']];
 }
 
-$sql_filtros = "SELECT DISTINCT cl.ciclo_id, cl.ciclo, c.carrera_id, c.carrera_nombre, t.turno_id, t.turno, cr.curso_id, cr.curso, p.profesor_id, CONCAT(p.profesor_apellido, ' ', p.profesor_nombre) AS profesor_nombre_completo
-    FROM materia_carrera mc
-    INNER JOIN ciclo_lectivo cl ON mc.ciclo_id = cl.ciclo_id
-    INNER JOIN turnos t ON mc.turno_id = t.turno_id
-    INNER JOIN carreras c ON mc.carrera_id = c.carrera_id
-    INNER JOIN cursos cr ON mc.curso_id = cr.curso_id
-    INNER JOIN profesores p ON mc.profesor_id = p.profesor_id
-    $where_sql
-    ORDER BY cl.ciclo, c.carrera_nombre, t.turno, cr.curso, p.profesor_apellido";
+// 3. Turnos (orden alfabético ascendente)
+$sql_turnos = "SELECT DISTINCT t.turno_id, t.turno 
+            FROM turnos t
+            INNER JOIN materia_carrera mc ON mc.turno_id = t.turno_id
+            ORDER BY t.turno ASC";
+$resultado_turnos = $con->query($sql_turnos);
+$turnos = [];
+while ($fila = $resultado_turnos->fetch_assoc()) {
+    $turnos[] = ['id' => $fila['turno_id'], 'descripcion' => $fila['turno']];
+}
 
+// 4. Cursos (orden alfabético ascendente)
+$sql_cursos = "SELECT DISTINCT cr.curso_id, cr.curso 
+            FROM cursos cr
+            INNER JOIN materia_carrera mc ON mc.curso_id = cr.curso_id
+            ORDER BY cr.curso ASC";
+$resultado_cursos = $con->query($sql_cursos);
+$cursos = [];
+while ($fila = $resultado_cursos->fetch_assoc()) {
+    $cursos[] = ['id' => $fila['curso_id'], 'descripcion' => $fila['curso']];
+}
 
-$resultado_filtros = $con->query($sql_filtros);
-$ciclos = $turnos = $carreras = $cursos = $profesores = [];
-
-while ($fila = $resultado_filtros->fetch_assoc()) {
-    if (!array_key_exists($fila['ciclo_id'], $ciclos)) {
-        $ciclos[$fila['ciclo_id']] = [
-            'id' => $fila['ciclo_id'],
-            'descripcion' => $fila['ciclo']
-        ];
-    }
-
-    if (!array_key_exists($fila['turno_id'], $turnos)) {
-        $turnos[$fila['turno_id']] = [
-            'id' => $fila['turno_id'],
-            'descripcion' => $fila['turno']
-        ];
-    }
-
-    if (!array_key_exists($fila['carrera_id'], $carreras)) {
-        $carreras[$fila['carrera_id']] = [
-            'id' => $fila['carrera_id'],
-            'descripcion' => $fila['carrera_nombre']
-        ];
-    }
-
-    if (!array_key_exists($fila['curso_id'], $cursos)) {
-        $cursos[$fila['curso_id']] = [
-            'id' => $fila['curso_id'],
-            'descripcion' => $fila['curso']
-        ];
-    }
-
-    if (!array_key_exists($fila['profesor_id'], $profesores)) {
-        $profesores[$fila['profesor_id']] = [
-            'id' => $fila['profesor_id'],
-            'descripcion' => $fila['profesor_nombre_completo']
-        ];
-    }
+// 5. Profesores (orden alfabético por apellido)
+$sql_profesores = "SELECT DISTINCT p.profesor_id, CONCAT(p.profesor_apellido, ' ', p.profesor_nombre) AS profesor_nombre_completo
+                FROM profesores p
+                INNER JOIN materia_carrera mc ON mc.profesor_id = p.profesor_id
+                ORDER BY p.profesor_apellido ASC";
+$resultado_profesores = $con->query($sql_profesores);
+$profesores = [];
+while ($fila = $resultado_profesores->fetch_assoc()) {
+    $profesores[] = ['id' => $fila['profesor_id'], 'descripcion' => $fila['profesor_nombre_completo']];
 }
 
 $ciclos = array_values($ciclos);
